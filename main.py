821e0ab8e87e86fd967521288998246ba27c76b8
Sharon Zou <43053716+szou00@users.noreply.github.com>
2021-06-29 11:14:56 -0400

    basic functionality works

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
-# from flask.signals import Namespace
-# from flask_sqlalchemy import SQLAlchemy
+# Reminder Application 
+# Authors: Sharon, Hailey, Mayank
 
+# import modules
+from flask import Flask, redirect, url_for, render_template, request
+
+# app configurations 
 app = Flask(__name__)
 app.secret_key = "reminder-app"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.sqlite3'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# app.permanent_session_lifetime = timedelta(day=1)
 
-@app.route("/")
+# home page. reminders are added here 
+@app.route("/", methods=["POST","GET"])
 def home():
+    if request.method == "POST":
+        reminderName = request.form["rname"]
+        reminderTime = request.form["time"]
+        return redirect(url_for("view",name=reminderName, time=reminderTime))
     return render_template("index.html")
 
+# view reminders page.
+@app.route("/view", methods=["POST","GET"])
+def view():
+    if request.method == "POST":
+        return redirect(url_for("home"))
+    return render_template("view.html",name=request.args.get("name"),time=request.args.get('time'))
+
+# run the app
 if __name__ == "__main__":
     app.run(debug=True)
 
